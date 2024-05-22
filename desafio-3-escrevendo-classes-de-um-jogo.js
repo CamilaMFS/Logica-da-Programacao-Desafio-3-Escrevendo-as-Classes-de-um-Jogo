cfc4dec8632e1ace918a839064a58c565bf95b14
Camila Marques <mfs.cami@gmail.com>
2024-05-22 16:22:56 -0300

    Add files via upload

---
+++ desafio-3-escrevendo-classes-de-um-jogo.js
@@ -0,0 +1,29 @@
+class Heroi {
+    constructor(nome, idade, tipo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+    }
+    atacar() {
+        let ataque;
+        switch (this.tipo) {
+            case 'mago':
+                ataque = 'explosion';
+                break;
+            case 'guerreiro':
+                ataque = 'espada';
+                break;
+            case 'monge':
+                ataque = 'artes marciais';
+                break;
+            case 'ninja':
+                ataque = 'shuriken';
+                break;
+            default:
+                ataque = 'realizou um ataque';
+        }
+        console.log(`O ${this.tipo} Megumim atacou usando ${ataque}`);
+    }
+}
+const heroi1 = new Heroi('Megumim', 30, 'mago');
+heroi1.atacar();
